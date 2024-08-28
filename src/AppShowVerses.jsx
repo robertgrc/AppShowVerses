6c9f74b752201b35cd8f56fc79e8f36794126772
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-08-28 16:33:54 -0400

    Nuevo Componente para mostrar las categorias

--- src/AppShowVerses.jsx
+++ src/AppShowVerses.jsx
@@ -1,28 +1,31 @@
 import React, { useState } from 'react'
 import { AddCategory } from './components/AddCategory'
+import { GifGrid } from './components/GifGrid'
 
 export const AppShowVerses = () => {
 
 const [categories, setCategories] = useState(['fear of God','seed of god', 'Mesias'])
 
 const onAddCategory=( newCategory)=>{
-    console.log(newCategory)
+   
+    if( categories.includes(newCategory)) return
+
     setCategories([newCategory, ...categories])
 }
 
   return (
     <>
         <h1>AppShowVerses</h1>
-        {/*Listado de Imagenes*/}
         <AddCategory 
-            // setCategories={setCategories} 
             onNewCategory = {value=> onAddCategory(value)}
         />
-        <ol>
-            {categories.map(category => {
-                return <li key={category}>{category} </li>
-            })}
-        </ol>
+            {categories.map(category => 
+            <GifGrid  
+                key={category} 
+                category={category}
+            />
+        )
+            }
     </>
   )
 }
